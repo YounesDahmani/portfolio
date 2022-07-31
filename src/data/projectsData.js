67077f52d3313e262626b849336b40c6a119e2df
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-07-31 20:29:04 +0200

    realization of the Works page

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -43,4 +43,26 @@ export const projectsData = [
     img: "./assets/img/country-app.png",
     link: "http://www.google.com",
   },
+  {
+    id: 5,
+    number: "05",
+    title: "My resto",
+    date: "Juillet 2020",
+    languages: ["HTML", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/country-app.png",
+    link: "http://www.google.com",
+  },
+  {
+    id: 6,
+    number: "05",
+    title: "Country App",
+    date: "Juillet 2020",
+    languages: ["Javascript", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/country-app.png",
+    link: "http://www.google.com",
+  },
 ];
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,11 +1,45 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Project from "../components/Project";
 
 const Works = () => {
   return (
-    <div>
+    <div className="works">
       <Navbar />
-      <h1>Works</h1>
+
+      {/* -------- HEADER ------------ */}
+
+      <div className="works-header">
+        <h1>My Works.</h1>
+        <i class="fa-solid fa-angle-down"></i>
+      </div>
+
+      {/* ------------ PROJECTS -------------- */}
+      <div className="works-items">
+        <Project projectNumber={0} />
+        <Project projectNumber={1} />
+        <Project projectNumber={2} />
+        <Project projectNumber={3} />
+        <Project projectNumber={4} />
+      </div>
+
+      {/* ------------- GitHub projects ------------- */}
+
+      <div className="github-projects">
+        <h2>GitHub Projects.</h2>
+        <p>
+          Here are some of my projects that I created during my spare time. I
+          constantly keep on improving my skills by making these fun projects.
+          These projects are available on my <a href="#">github repository</a>
+        </p>
+      </div>
+
+      {/* --------------- FOOTER --------------- */}
+
+      <div className="works-footer">
+        <Footer />
+      </div>
     </div>
   );
 };
