e9c5ce0af624d54cbd40bd008d5013c5329ff241
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-07-31 21:26:03 +0200

    realization of the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,49 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navbar />
-      <h1>Contact</h1>
+      <div className="contact-container">
+        <h1>Get in touch.</h1>
+        <p>
+          Do you fancy saying hi to me or you want to get started with your
+          project and you need my help ? <br /> Feel free to contact me.
+        </p>
+
+        <div className="form-container">
+          <form className="form-content">
+            <input
+              type="text"
+              name="name"
+              required
+              autoComplete="off"
+              id="name"
+              placeholder="Your Name"
+              className="input-contact"
+            />
+            <input
+              type="email"
+              name="email"
+              required
+              autoComplete="off"
+              id="email"
+              placeholder="Your Email"
+              className="input-contact"
+            />
+            <textarea placeholder="Message" name="message" id="mess" />
+            <input
+              type="submit"
+              value="SEND MESSAGE"
+              className="hover button"
+            />
+          </form>
+          <div className="formMessage"></div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
